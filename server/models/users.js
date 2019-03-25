7d909e7697522f63d07f45433735790bd585555c
Edidiong Udombat <edidiongudombat@Edidiongs-MacBook-Pro.local>
2019-03-25 18:59:53 +0100

    [chore #164797136]Correct typographical error in variable naming in user model

--- server/models/users.js
+++ server/models/users.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
   const Users = sequelize.define('Users', {
-    ffirstName: {
+    firstName: {
       type: DataTypes.STRING,
       allowNull: false,
     },
