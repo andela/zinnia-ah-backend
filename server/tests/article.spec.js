f3bb2db42a2098b39202b8fb5d70f19a9f43b9b3
Favour Afolayan <favourafolayan@gmail.com>
2019-03-25 20:47:17 +0100

    [chore #1647971408] integrate mocha and chai

---
+++ server/tests/article.spec.js
@@ -0,0 +1,21 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../index';
+
+// configure chai to use expect
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe('Articles', () => {
+    describe('GET /api/v1/articles', () => {
+        it('returns a 404 error', async () => {
+            const response = await chai.request(app)
+                .get('/api/v1/articles');
+            expect(response.body).to.have.status(404);
+            expect(response.body).to.have.key('errors');
+            expect(response.body.errors).to.have.key('body');
+            expect(response.body.errors.body).to.not.be.empty();
+            expect(response.body.errors.body).to.be.an('array');
+        });
+    })
+});
