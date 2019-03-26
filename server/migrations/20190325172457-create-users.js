dd8581b23bc499f951d53302789bec99575b0b47
Edidiong Udombat <edidiongudombat@Edidiongs-MacBook-Pro.local>
2019-03-26 13:08:46 +0100

    chore(root, server) Implement PR feedback
    properly align contents of .babelrc file
    rename model and migration files to foloow proper naming coventions
    fix merge conflicts while rebasing
    
    [finish #164797136]

--- server/migrations/20190325172457-create-users.js
+++ server/migrations/20190325172457-create-users.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) =>
-    queryInterface.createTable('Users', {
+    queryInterface.createTable('User', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -28,5 +28,5 @@ module.exports = {
         type: Sequelize.DATE,
       },
     }),
-  down: (queryInterface /* , Sequelize */) => queryInterface.dropTable('Todos'),
+  down: (queryInterface /* , Sequelize */) => queryInterface.dropTable('User'),
 };
