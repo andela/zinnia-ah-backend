d619b5623add4d90b52623d8fb22aff53ea947db
Emmanuel Daniel <emmanuel.daniel@andela.com>
2019-03-28 11:14:38 +0100

    [feat #164797142] configured api documentation with swagger  - custom theme for swagger documentation  - auto-generate documentation from route comments

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,42 @@ import welcome from './controllers';
 
 const router = Router();
 
+/**
+ * @swagger
+ * definition:
+ *    default:
+ *    users:
+ *    profiles:
+ *    articles:
+ *    tags:
+ */
+/**
+ * @swagger
+ *
+ * /api/v1/welcome:
+ *   get:
+ *     description: Default Welcome Message
+ *     produces:
+ *       - application/json
+ *     request:
+ *         content:
+ *         - application/json
+ *         schema:
+ *           type: array
+ *           items:
+ *         $ref: '#/definitions/default'
+ *     responses:
+ *       200:
+ *         description: welcome message
+ *       400:
+ *         description: Bad request.
+ *       401:
+ *         description: Authorization information is missing or invalid.
+ *       404:
+ *        description: A user with the specified ID was not found.
+ *       5XX:
+ *        description: Unexpected error.
+ */
 router.get('/welcome', welcome);
 
 export default router;
