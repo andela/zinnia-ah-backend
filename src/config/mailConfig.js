5deccef474a229271f4ea8482297285c65c8801e
nedyudombat <nedyudombat@gmail.com>
2019-04-01 00:28:27 +0100

    [chore #164957460] Setup email configuration
    Install nodemailer
    create a configuration file for email that uses nodemailer and smtp
    add smtp details example to enviroment example file
    add IDE folder to gitignore
    
    [start #164957469]

---
+++ src/config/mailConfig.js
@@ -0,0 +1,41 @@
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const email = process.env.EMAIL;
+const host = process.env.SMTP_HOST;
+const port = process.env.SMTP_PORT;
+const username = process.env.SMTP_USERNAME;
+const pass = process.env.SMTP_MAIL_PASSWORD;
+
+const transporter = nodemailer.createTransport({
+  host,
+  port,
+  auth: {
+    user: username,
+    pass
+  }
+});
+
+const sendMail = (req, res, body) => {
+  console.log(body.subject, body.receiver);
+
+  const allRecipientEmail = body.receiver.join(', ');
+  const mailOptions = {
+    from: email,
+    to: allRecipientEmail,
+    subject: body.subject,
+    text: body.text,
+    html: body.html
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.json({ error });
+    }
+    return res.json({ info });
+  });
+};
+
+export default sendMail;
