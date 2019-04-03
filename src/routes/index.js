52322c77f1eeefab8a357082b024c112885f36aa
Emmanuel Osuh <mrmusonant@gmail.com>
2019-04-03 20:51:49 +0100

    Add passport-facebook configuration

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,4 +12,13 @@ router.use('/auth/users', passwordRouter);
 
 router.use('/article', articleRouter);
 
+router.get('/auth/facebook', passport.authenticate('facebook'));
+router.get(
+  '/auth/facebook/callback',
+  passport.authenticate('facebook', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+  }),
+);
+
 export default router;
