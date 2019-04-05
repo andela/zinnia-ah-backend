889bc30113e12443bb046c9b6a30904e3cb0a116
Favour Afolayan <favourafolayan@gmail.com>
2019-04-05 13:22:08 +0100

    [chore] add seeders for articles and users

--- src/db/models/User.js
+++ src/db/models/User.js
@@ -1,78 +1,82 @@
 import bcrypt from 'bcryptjs';
 
 export default (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    id: {
-      type: DataTypes.UUID,
-      allowNull: false,
-      primaryKey: true,
-      defaultValue: DataTypes.UUIDV4,
-    },
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'first_name',
-    },
-    lastName: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'last_name',
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      unique: true,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    bio: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
-    interests: {
-      type: DataTypes.ARRAY(DataTypes.TEXT),
-      allowNull: true,
-    },
-    image: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
-    isEmailVerified: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-      allowNull: true,
-    },
-    createdAt: {
-      type: DataTypes.DATE,
-      field: 'created_at',
-    },
-    socialProvider: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'social_provider',
-    },
-    socialId: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'social_id',
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      field: 'updated_at',
+  const User = sequelize.define(
+    'User',
+    {
+      id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        field: 'first_name',
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        field: 'last_name',
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        unique: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      bio: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      interests: {
+        type: DataTypes.ARRAY(DataTypes.TEXT),
+        allowNull: true,
+      },
+      image: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      isEmailVerified: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: true,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        field: 'created_at',
+      },
+      socialProvider: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        field: 'social_provider',
+      },
+      socialId: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        field: 'social_id',
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        field: 'updated_at',
+      },
     },
     {
-    hooks: {
-      beforeCreate: user => user.password && user.hashPassword(),
-      beforeUpdate: user => user.password && user.hashPassword(),
+      hooks: {
+        beforeCreate: user => user.password && user.hashPassword(),
+        beforeUpdate: user => user.password && user.hashPassword(),
+      },
     },
-  });
+  );
   User.associate = () => {
     // associations can be defined here
     User.hasMany(model.Article);
--- src/db/seeders/20190405073146-article-seeder.js
+++ src/db/seeders/20190405073146-article-seeder.js
@@ -1,7 +1,9 @@
 import uuidv4 from 'uuidv4';
+import slug from 'slugify';
 
-module.exports = {
+export default {
   // eslint-disable-next-line no-return-await
+<<<<<<< HEAD
   up: async queryInterface =>
     await queryInterface.bulkInsert(
       'Users',
@@ -59,6 +61,60 @@ module.exports = {
       ],
       {},
     ),
+=======
+  up: async queryInterface => await queryInterface.bulkInsert('Articles', [
+    {
+      id: uuidv4(),
+      user_id: '5a6fab9c-5849-4be5-973c-5a371165cd57',
+      title: 'Hello Article',
+      slug: slug(`Hello Article 1-${uuidv4()}`),
+      description: 'Description goes here',
+      body: 'Another Body',
+      created_at: new Date().toLocaleString(),
+      updated_at: new Date().toLocaleString(),
+    },
+    {
+      id: uuidv4(),
+      user_id: '5a6fab9c-5849-4be5-973c-5a371165cd57',
+      title: 'Hello Article 2',
+      slug: slug(`Hello Article 2-${uuidv4()}`),
+      description: 'Description goes here',
+      body: 'Another Body',
+      created_at: new Date().toLocaleString(),
+      updated_at: new Date().toLocaleString(),
+    },
+    {
+      id: uuidv4(),
+      user_id: '5a6fab9c-5849-4be5-973c-5a371165cd57',
+      title: 'Hello Article 3',
+      slug: slug(`Hello Article 3-${uuidv4()}`),
+      description: 'Description goes here',
+      body: 'Another Body',
+      created_at: new Date().toLocaleString(),
+      updated_at: new Date().toLocaleString(),
+    },
+    {
+      id: uuidv4(),
+      user_id: '5a6fab9c-5849-4be5-973c-5a371165cd57',
+      title: 'Hello Article 4',
+      slug: slug(`Hello Article 4-${uuidv4()}`),
+      description: 'Description goes here',
+      body: 'Another Body',
+      created_at: new Date().toLocaleString(),
+      updated_at: new Date().toLocaleString(),
+    },
+    {
+      id: uuidv4(),
+      user_id: '5a6fab9c-5849-4be5-973c-5a371165cd57',
+      title: 'Hello Article 5',
+      slug: slug(`Hello Article 5-${uuidv4()}`),
+      description: 'Description goes here',
+      body: 'Another Body',
+      created_at: new Date().toLocaleString(),
+      updated_at: new Date().toLocaleString(),
+    }
+  ], {}),
+>>>>>>> [chore] add seeders for articles and users
 
   down: async queryInterface => queryInterface.bulkDelete('Articles', null, {}),
 };
