d01dd4c634c9e72645c8b2cc2ea9ceacc64565c1
Emmanuel Osuh <42358974+musonant@users.noreply.github.com>
2019-04-08 11:50:46 +0100

    Make facebook callback an environment variable

--- src/routes/services/passport-strategies.js
+++ src/routes/services/passport-strategies.js
@@ -11,7 +11,7 @@ const credentials = {
   facebook: {
     clientID: process.env.FACEBOOK_APP_CLIENT_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: 'http://localhost:3000/api/v1/auth/facebook/callback',
+    callbackURL: process.env.FACEBOOK_APP_CALLBACK,
     profileFields: ['id', 'email', 'name'],
   }
 };
