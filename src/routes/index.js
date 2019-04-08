2ababf2184241bbfcac70df873673502f1db306a
nedy udombat <nedyudombat@gmail.com>
2019-04-08 10:07:14 +0100

    #164797146 Reset password reset via email (#20)
    * [feature #164797146]Implement reset password fucntionality
    
    write test for forgot password
    implemement forgot password to send mail to user
    write test for reset password
    implement reser password to update new password
    
    [start #164797146]
    
    * [feature #164797146]Implement PR feedback
    
    * [feature #164797146]Implement hound feedback
    
    * [feature #164797146]Add JSDoc to resetpassword function
    
    * [feature #164797146]use helper functions
    
    * [feature #164797146]removed unused functions
    
    * [feature #164797146]rebasing from develop

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
+import { forgotPassword, resetPassword } from './controllers/password';
 import { createUser, confirmUser, } from './controllers/users';
-
 import validateNewUser from './middlewares/validateUser';
 
 
@@ -44,5 +44,65 @@ const router = Router();
  */
 router.post('/users', validateNewUser, createUser);
 router.get('/users/confirmation/:token', confirmUser);
+
+
+/**
+ * @swagger
+ *
+ * /api/v1/users:
+ *   post:
+ *     description: Forgot Password
+ *     produces:
+ *       - application/json
+ *     request:
+ *         content:
+ *         - application/json
+ *         schema:
+ *           type: array
+ *           items:
+ *         $ref: '#/definitions/users'
+ *     responses:
+ *       201:
+ *         description: User created
+ *       400:
+ *         description: Bad request.
+ *       401:
+ *         description: Authorization information is missing or invalid.
+ *       404:
+ *        description: A user with the specified ID was not found.
+ *       5XX:
+ *        description: Unexpected error.
+ */
+router.post('/users/forgot-password', forgotPassword);
+
+
+/**
+ * @swagger
+ *
+ * /api/v1/users:
+ *   post:
+ *     description: Reset Password
+ *     produces:
+ *       - application/json
+ *     request:
+ *         content:
+ *         - application/json
+ *         schema:
+ *           type: array
+ *           items:
+ *         $ref: '#/definitions/users'
+ *     responses:
+ *       201:
+ *         description: User created
+ *       400:
+ *         description: Bad request.
+ *       401:
+ *         description: Authorization information is missing or invalid.
+ *       404:
+ *        description: A user with the specified ID was not found.
+ *       5XX:
+ *        description: Unexpected error.
+ */
+router.patch('/users/reset-password/:token', resetPassword);
 
 export default router;
