a5efb1a7b4c34acf238ce9af8ec62742b99cecf5
Favour Afolayan <favourafolayan@gmail.com>
2019-04-08 09:45:56 +0100

    [chore] add more limits test

--- src/tests/db/controller/article.test.js
+++ src/tests/db/controller/article.test.js
@@ -10,22 +10,35 @@ describe('Articles', () => {
   describe('GET /api/v1/articles', () => {
     it('should return a 200 response when articles exist', async () => {
       const response = await chai.request(app).get(endPoint);
-
       expect(response.body).to.include.keys('status', 'message', 'data');
       expect(response.status).to.eql(200);
       expect(response.body.status).to.eql('success');
       expect(response.body.message).to.eql('Articles successfully retrieved');
-      expect(response.body.data.length).to.be.greaterThan(0);
+      expect(response.body.data.rows.length).to.be.greaterThan(0);
     });
 
-    it('should return the exact number of articles set by limit', async () => {
-      const response = await chai.request(app).get(endPoint);
-
+    it('should return 10 articles when limit is set to 10', async () => {
+      const limit = 10;
+      const response = await chai
+        .request(app)
+        .get(endPoint)
+        .query({ limit });
       expect(response.body).to.include.keys('status', 'message', 'data');
       expect(response.status).to.eql(200);
       expect(response.body.status).to.eql('success');
-      expect(response.body.data.length).to.be.greaterThan(0);
-      expect(response.body.data.length).to.be.greaterThan(0);
+      expect(response.body.data.rows.length).to.eql(limit);
+    });
+
+    it('should return 20 articles when limit is set to 20', async () => {
+      const limit = 20;
+      const response = await chai
+        .request(app)
+        .get(endPoint)
+        .query({ limit });
+      expect(response.body).to.include.keys('status', 'message', 'data');
+      expect(response.status).to.eql(200);
+      expect(response.body.status).to.eql('success');
+      expect(response.body.data.rows.length).to.eql(limit);
     });
   });
 });
