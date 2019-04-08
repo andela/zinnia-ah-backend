bde2a23781bc4a27b7f5afc59cbed4555139c383
Favour Afolayan <favourafolayan@gmail.com>
2019-04-08 09:32:18 +0100

    [feature] complete article pagination

---
+++ src/tests/db/controller/user.test.js
@@ -0,0 +1,59 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import models from '../../../db/models';
+import app from '../../../server';
+
+// configure chai to use expect
+chai.use(chaiHttp);
+const { expect } = chai;
+const url = '/api/v1/users';
+const confirmationUrl = '/api/v1/users/confirmation';
+const userRequestObject = {
+  username: 'janesmith',
+  email: 'jsmith@gmail.com',
+  password: 'hhrtuyhgty5t678',
+};
+
+describe('CREATE USER', () => {
+  let userToken;
+  it('should create a user successfully when valid input are supplied', done => {
+    chai
+      .request(app)
+      .post(url)
+      .send(userRequestObject)
+      .end((err, res) => {
+        expect(res.status).to.equal(201);
+        userToken = res.body.data[0].token;
+        done();
+      });
+  });
+  it('should confirm a user', done => {
+    chai
+      .request(app)
+      .get(`${confirmationUrl}/${userToken}`)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        done();
+      });
+  });
+  it('should fail creation when email is already in use', done => {
+    chai
+      .request(app)
+      .post(url)
+      .send(userRequestObject)
+      .end((err, res) => {
+        expect(res.status).to.equal(409);
+        done();
+      });
+  });
+  it('should fail creation when username is already in use', done => {
+    chai
+      .request(app)
+      .post(url)
+      .send(userRequestObject)
+      .end((err, res) => {
+        expect(res.status).to.equal(409);
+        done();
+      });
+  });
+});
