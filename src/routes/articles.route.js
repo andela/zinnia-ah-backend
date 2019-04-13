11ce74ff3734c61933bb9f249d399baabfa05cb6
Emmanuel Osuh <mrmusonant@gmail.com>
2019-04-13 13:02:47 +0100

    Add route for rating an article

--- src/routes/articles.route.js
+++ src/routes/articles.route.js
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 
-import { validateUuid } from './middlewares/validate-input.middleware';
+import {
+  validateUuid,
+  validateRating,
+} from './middlewares/validate-input.middleware';
 import {
   createComment,
   createThreadedComment,
@@ -17,6 +20,7 @@ import {
   bookmarkArticle,
   removeBookmark,
   reportArticle,
+  rateArticle,
 } from './controllers/articles.controller';
 import checkAuthorizedUser from './middlewares/authorized-user.middleware';
 
@@ -211,6 +215,12 @@ articleRouter.post(
  *         description: Database error
  */
 articleRouter.get('/:articleId', validateUuid, getArticle);
+articleRouter.post(
+  '/:articleId/rate',
+  checkAuthorizedUser,
+  validateRating,
+  rateArticle,
+);
 
 /**
  * @swagger
