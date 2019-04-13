34c0d99123abe5010f1b4ccde8c8321110e41f2f
Emmanuel Osuh <mrmusonant@gmail.com>
2019-04-13 13:02:47 +0100

    Add route for rating an article

--- src/routes/articles.route.js
+++ src/routes/articles.route.js
@@ -23,6 +23,10 @@ import {
   reportArticle,
   rateArticle,
 } from './controllers/articles.controller';
+import {
+  validateUuid,
+  validateRating,
+} from './middlewares/validate-input.middleware';
 import checkAuthorizedUser from './middlewares/authorized-user.middleware';
 
 const articleRouter = Router();
