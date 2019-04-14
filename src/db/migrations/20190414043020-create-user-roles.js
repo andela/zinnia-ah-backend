e0aa5e3bdf25859e3e6df958835b45a35cf26683
Favour Afolayan <favourafolayan@gmail.com>
2019-04-14 10:16:20 +0100

    [feature] setup roles endpoint and restrict access

--- src/db/migrations/20190414043020-create-user-roles.js
+++ src/db/migrations/20190414043020-create-user-roles.js
@@ -26,12 +26,10 @@ export default {
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        field: 'created_at',
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        field: 'updated_at',
       },
     });
   },
--- src/tests/routes/middlewares/roles.test.js
+++ src/tests/routes/middlewares/roles.test.js
@@ -34,49 +34,41 @@ describe('Roles and Access control', () => {
     it('should return a 401 when token is not set', async () => {
       const { status, body } = await chai.request(app).get(rolesEndpoint);
 
-      expect(status).to.be.eql(401);
-      expect(body).to.have.key('errors', 'status', 'message');
+      expect(status).to.eql(401);
+      expect(body).to.have.key('status', 'message');
       expect(body.status).to.eql('error');
-      expect(body.message).to.eql('Unauthorized access');
+      expect(body.message).to.eql('Please provide a JWT token');
     });
 
-    it('should return a 401 when token is invalid', async () => {
+    it('should return a 400 when token is invalid', async () => {
       const { status, body } = await chai
         .request(app)
-        .post(rolesEndpoint)
+        .get(rolesEndpoint)
         .set('x-access-token', 'jwt-token');
 
-      expect(status).to.be.eql(401);
-      expect(body).to.have.key('errors', 'status', 'message');
+      expect(status).to.eql(400);
+      expect(body).to.have.key('status', 'message');
       expect(body.status).to.eql('error');
-      expect(body.message).to.eql('Unauthorized access');
+      expect(body.message).to.eql(
+        'Token is invalid, please provide a valid token',
+      );
     });
 
     it('should return a 403 response when an unauthorized user tries to access an admin only route', async () => {
       const { status, body } = await chai
         .request(app)
-        .post(rolesEndpoint)
-        .set('x-acess-token', userToken);
+        .get(rolesEndpoint)
+        .set('x-access-token', userToken);
 
       expect(status).to.be.eql(403);
-      expect(body).to.have.key('errors', 'status', 'message');
+      expect(body).to.have.key('status', 'message');
       expect(body.status).to.eql('error');
       expect(body.message).to.eql(
-        'Your credentials do not allow access to this resource',
+        'You are not allowed to perform this action because you are not an Admin',
       );
     });
-  });
-  describe('roles assignment', () => {
-    before(async () => {
-      const { body } = await chai
-        .request(app)
-        .post('/api/v1/auth/login')
-        .send(adminCredentials);
 
-      adminToken = body.data;
-    });
-
-    it('should return a 200 response and an array of user roles when request user is an admin', async () => {
+    it('should return a 200 response when an authorized user accesses a protected route', async () => {
       const { status, body } = await chai
         .request(app)
         .get(rolesEndpoint)
