ffa9133a85e57d25d2d50d292f070e4d9a80f745
Favour Afolayan <favourafolayan@gmail.com>
2019-04-14 10:39:59 +0100

    [chore] add route documentation

--- src/routes/roles.route.js
+++ src/routes/roles.route.js
@@ -6,6 +6,33 @@ import verifyAdminUser from './middlewares/verify-admin.middleware';
 
 const rolesRouter = Router();
 
+/**
+ * @swagger
+ *
+ * /api/v1/roles:
+ *   get:
+ *     description: Get roles
+ *     produces:
+ *       - application/json
+ *     request:
+ *         content:
+ *         - application/json
+ *         schema:
+ *           type: array
+ *           items:
+ *         $ref: '#/definitions/roles'
+ *     responses:
+ *       200:
+ *         description: Request successful
+ *       400:
+ *         description: Bad request.
+ *       401:
+ *         description: Authorization information is missing or invalid.
+ *       403:
+ *        description: Access Forbidden.
+ *       5XX:
+ *        description: Unexpected error.
+ */
 rolesRouter.get('/', checkAuthorizedUser, verifyAdminUser, getAll);
 
 export default rolesRouter;
