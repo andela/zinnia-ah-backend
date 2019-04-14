b2741233b9f705c627dbfe55043c20aa5e83388d
Favour Afolayan <favourafolayan@gmail.com>
2019-04-14 06:06:18 +0100

    [chore] setup migrations for roles

---
+++ src/db/migrations/20190414042938-create-roles.js
@@ -0,0 +1,26 @@
+export default {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Roles', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'created_at',
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'updated_at',
+      },
+    });
+  },
+  down: queryInterface => queryInterface.dropTable('Roles'),
+};
