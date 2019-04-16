502bcf8fc69eefeb4b4835af5cca2ff29e4d237f
Emmanuel Osuh <mrmusonant@gmail.com>
2019-04-16 14:03:30 +0100

    Setup controller for rating

--- src/routes/controllers/articles.controller.js
+++ src/routes/controllers/articles.controller.js
@@ -337,7 +337,7 @@ export async function removeBookmark(req, res) {
       userData,
     });
   } catch (error) {
-    return errorResponse(res, 500, error.message);
+    return errorResponse(res, 500, 'An error occurred', error.message);
   }
 }
 
