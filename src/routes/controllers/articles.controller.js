313158db1fcb727345bb8b059f041a2f96276cce
Emmanuel Osuh <mrmusonant@gmail.com>
2019-04-16 14:03:30 +0100

    Setup controller for rating

--- src/routes/controllers/articles.controller.js
+++ src/routes/controllers/articles.controller.js
@@ -446,6 +446,7 @@ export async function getAllArticles(req, res) {
     return serverError(res);
   }
 }
+
 export const rateArticle = async (req, res) => {
   const { rating } = req.body;
   const { articleId } = req.params;
