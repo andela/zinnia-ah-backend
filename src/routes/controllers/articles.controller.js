527a1e6ae4ccf894e91cf690ab33a7347aa260f6
Emmanuel Osuh <mrmusonant@gmail.com>
2019-04-16 14:03:30 +0100

    Setup controller for rating

--- src/routes/controllers/articles.controller.js
+++ src/routes/controllers/articles.controller.js
@@ -331,10 +331,38 @@ export async function removeBookmark(req, res) {
     return errorResponse(res, 500, 'An error occurred', error.message);
   }
 }
+
 export const rateArticle = async (req, res) => {
   const { rating } = req.body;
   const { articleId } = req.params;
   const userId = req.user.id;
 
-  return errorResponse(res, 400, 'Nothing happened');
+  try {
+    const createdRating = await Rating.findOrCreate({
+      where: { articleId, userId },
+      defaults: { rating },
+    });
+
+    const ratedArticle = await Article.findByPk(articleId, {
+      attributes: {
+        exclude: ['id', 'userId', 'subscriptionType', 'readTime'],
+      },
+      include: [
+        {
+          model: Rating,
+          as: 'ratings',
+          attributes: ['rating', 'userId'],
+        },
+      ],
+    });
+
+    return successResponse(
+      res,
+      201,
+      'your rating has been recorded',
+      ratedArticle,
+    );
+  } catch (error) {
+    return errorResponse(res, 500, 'An error occured', error.message);
+  }
 };
